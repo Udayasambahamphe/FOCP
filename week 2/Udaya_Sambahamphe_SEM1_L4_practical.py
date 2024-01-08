b65aa3a47abc91c217899f91c2a1297f6b763dbb
Udayasambahamphe <subbaudaya094@gmail.com>
2024-01-08 11:26:02 +0545

    Assignemnts

--- week 2/Udaya_Sambahamphe_SEM1_L4_practical.py
+++ week 2/Udaya_Sambahamphe_SEM1_L4_practical.py
@@ -1,13 +1,13 @@
-#1
+#Qn no 1
 user=input("Hello what is your name? ")
 print(f"Hello {user}. Nice to meet you." )
 
-#2
+#Qn no 2
 celsius = int(input("Enter temperature in celsius: "))
 fahrenheit = (celsius * 9/5) + 32
 print(f"{celsius}.C is equivalent to {fahrenheit}.F")
 
-#3
+#Qn no 3
 Students = int(input("Number of Students : "))
 req_size = int(input("Required group size :  "))
 num_of_group = Students // req_size
@@ -23,7 +23,7 @@ if rem_students == 0:
 else:
     print(f"There will be {num_of_group} {group_word} with {rem_students} student{'s' if rem_students > 1 else ''} left.")
 
-# 4
+# Qn no 4
 tub_of_sweets = int(input("Enter total number of sweets: "))
 pupils = int(input("Enter total number of pupils: "))
 sweet_pupils = tub_of_sweets // pupils
